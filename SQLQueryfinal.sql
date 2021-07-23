25df8eabddbf804005e12c8d02b4bb6ccb5642cf
Karina Varma <70440854+karinavarma08@users.noreply.github.com>
2021-07-23 21:49:40 +0530

    Update SQLQueryfinal.sql

--- SQLQueryfinal.sql
+++ SQLQueryfinal.sql
@@ -186,24 +186,24 @@ Queries used for Tableau Project
 
 -- 1. 
 
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-where continent is not null 
---Group By date
-order by 1,2
+SELECT SUM(new_cases) as newtotal_cases, SUM(cast(new_deaths as int)) as total_Deaths, SUM(cast(new_deaths as int))/SUM(new_Cases)*100 as DeathPercentage
+FROM PortfolioProjects..['CDeaths$']
+--WHERE location like '%states%'
+WHERE continent is not null 
+--GROUP BY date
+ORDER BY 1,2
 
 
 
 ---- Just a double check based off the data provided
 ---- numbers are extremely close so we will keep them - The Second includes "International"  Location
 
---Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where location = 'World'
-----Group By date
---order by 1,2
+SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_Deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+FROM PortfolioProjects..['CDeaths$']
+WHERE location like '%states%'
+--WHERE location = 'World'
+GROUP BY date
+ORDER BY 1,2
 
 
 -- 2. 
@@ -211,30 +211,30 @@ order by 1,2
 ---- We take these out as they are not inluded in the above queries and want to stay consistent
 ----European Union is part of Europe
 
-Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-Where continent is null 
-and location not in ('World', 'European Union', 'International')
-Group by location
-order by TotalDeathCount desc
+SELECT location, SUM(cast(new_deaths as int)) as TotalDeathCount
+FROM PortfolioProjects..['CDeaths$']
+--WHERE location like '%states%'
+WHERE continent is null 
+AND location not in ('World', 'European Union', 'International')
+GROUP BY location
+ORDER BY TotalDeathCount desc
 
 
 -- 3.
 
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-Group by Location, Population
-order by PercentPopulationInfected desc
+SELECT location, population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+FROM PortfolioProjects..['CDeaths$']
+--WHERE location like '%states%'
+GROUP BY Location, Population
+ORDER BY PercentPopulationInfected desc
 
 
 -- 4.
 
 
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+SELECT Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+FROM PortfolioProjects..['CDeaths$']
 --Where location like '%states%'
-Group by Location, Population, date
-order by PercentPopulationInfected desc
+GROUP BY Location, Population, date
+ORDER BY PercentPopulationInfected desc
 
